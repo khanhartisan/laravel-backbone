43fd3d7f84775acf371903ce70cdc9ff361ce115
Khanh <khanh.artisan@gmail.com>
2023-06-29 22:49:58 +0700

    update Record facade

--- src/Support/Facades/Counter/Recorder.php
+++ src/Support/Facades/Counter/Recorder.php
@@ -7,9 +7,12 @@ use KhanhArtisan\LaravelBackbone\Contracts\Counter\Interval;
 use KhanhArtisan\LaravelBackbone\Contracts\Counter\ShardKey;
 
 /**
- * @method bool record(string $partitionKey, Interval $interval, string|int $reference, int $value, int $shardSize = 1000, ?int $eventTime)
+ * @method bool record(string $partitionKey, Interval $interval, string|int $reference, int $value, int $shardSize = 1000, ?int $eventTime = null)
  * @method null|ShardKey getShardKeyForReference(string $partitionKey, Interval $interval, int $time, string|int $reference)
- * @method int // TODO: Continue from here
+ * @method int getMaxShardKeyLength(string $partitionKey, Interval $interval, int $time)
+ * @method int getShardSize(string $partitionKey, Interval $interval, int $time, ShardKey $shardKey)
+ * @method array getShardRecords(string $partitionKey, Interval $interval, int $time, ShardKey $shardKey)
+ * @method bool flush(string $partitionKey, ?Interval $interval, ?int $time = null, ?ShardKey $shardKey = null)
  */
 class Recorder extends Facade
 {
